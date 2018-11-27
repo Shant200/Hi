a3cf65040a50afbf50e3553d09bab9f26010b3c1
Shant200 <45374708+Shant200@users.noreply.github.com>
2018-11-27 12:01:27 -0600

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,17 @@
+function bg () {
+let viv = document.getElementById('div1');
+viv.style.backgroundColor ='pink';
+
+}
+const createtext = () => {
+ let viv = document.getElementById('div2');
+ let na= document.createElement('na');
+ na.innerText ='hi';
+ na.classList.add('bubble');
+ viv.appendChild(p);
+}
+const changebackground = () =>{
+ let heading = document.getElementById('nao');
+ heading.classList.add('nam');
+}
+
